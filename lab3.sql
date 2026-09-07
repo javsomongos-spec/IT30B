26e4546e3963ba35ddb3dbe71841baad366e2d1e
javsomongos-spec <javsomongos@gmail.com>
2026-09-08 07:53:31 +0800

    commits

--- lab3.sql
+++ lab3.sql
@@ -51,11 +51,14 @@ ORDER BY br.borrow_date DESC;
 
 ALTER TABLE borrow MODIFY borrow_return_date TIMESTAMP NULL DEFAULT NULL;
 
-UPDATE borrow SET borrow_return_date = NULL WHERE borrow_return_date = " 2026-08-25 09:45:52";
+UPDATE borrow SET borrow_return_date = NULL WHERE borrow_return_date = "2026-08-25 09:45:52";
 
-UPDATE borrow SET borrow_return_date = CURRENT_TIMESTAMP WHERE borrow_id= 1 AND BORROW_RETURN_DISATE  NULL;
-
+SELECT borrow_id, borrow_date, borrow_return_date
+FROM borrow
+WHERE borrow_id = 4;
 SELECT br.borrow_id,
+
+
     CONCAT(s. student_first_name, ' ', s.student_last_name) AS student_name,
     b.book_title, b.book_author, b.book_category,
     br.borrow_date, br.borrow_return_date
@@ -66,3 +69,16 @@ JOIN books b ON br.book_id = b.book_id
 
 WHERE br.borrow_return_date IS NOT NULL
 ORDER BY br.borrow_return_date DESC;
+
+
+SELECT br.borrow_id,
+       CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+       b.book_title,
+       b.book_author,
+       b.book_category,
+       br.borrow_date,
+       br.borrow_return_date
+FROM borrow br
+JOIN student s ON br.student_id = s.student_id
+JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_id = 4;
